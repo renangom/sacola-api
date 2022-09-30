e51b130c766aaf6b9d647c0dbb2c92611d5289b3
Renan <renan23eduardo@gmail.com>
2022-09-30 16:47:26 -0300

    Create addItem service

---
+++ src/main/java/me/dio/sacola/resource/SacolaResource.java
@@ -0,0 +1,30 @@
+package me.dio.sacola.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import me.dio.sacola.model.Item;
+import me.dio.sacola.model.Sacola;
+import me.dio.sacola.service.SacolaService;
+
+@RestController
+@RequestMapping("/ifood-devweek/sacolas")
+@RequiredArgsConstructor
+public class SacolaResource {
+    private final SacolaService sacolaService;
+
+    @PostMapping
+    public Item incluirItemNaSacola(@RequestBody ItemDto itemDto) {
+        return sacolaService.incluirItemNaSacola(itemDto);
+    }
+
+    @GetMapping
+    public Sacola verSacola(@RequestBody Long id) {
+
+    }
+
+}
